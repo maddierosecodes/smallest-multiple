3fed4e9d3ad9e9e65d70fa802a9de1dc17dddc55
Madeleine Hughes <maddierosecodes@github.com>
2022-06-24 21:57:24 +0100

    initial solution pending refactor

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const findSmallestMultiple = (upperDivisorLimit) => {
   if (upperDivisorLimit > -1 && upperDivisorLimit < 3) return upperDivisorLimit;
+  // Create array of divisors between 1 and upperDivisorLimit
   const divisorArray = Array.from(
     { length: upperDivisorLimit },
     (value, i) => i + 1
   );
-  // HCF = 1
-
-  // Find LCM
-  // [range*]/HCF(range)
+  let smallestMultiple = upperDivisorLimit
+  while (true) {
+    if (divisorArray.every((divisor) => smallestMultiple % divisor === 0 )) return smallestMultiple
+    smallestMultiple += upperDivisorLimit
+  }
 };
 
 module.exports = { findSmallestMultiple };
--- main.test.js
+++ main.test.js
@@ -8,4 +8,10 @@ describe('findSmallestMultiple', () => {
   it('returns the smallest multiple (6) when upper limit of divisors is 3', () => {
     expect(findSmallestMultiple(3)).toBe(6);
   });
+  it("returns the smallest multiple (6) when upper limit of divisors is larger than 3", () => {
+    expect(findSmallestMultiple(4)).toBe(12);
+    expect(findSmallestMultiple(10)).toBe(2520);
+    expect(findSmallestMultiple(20)).toBe(232792560);
+    
+  });
 });
